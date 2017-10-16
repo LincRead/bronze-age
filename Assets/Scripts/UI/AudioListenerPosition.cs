7b928587bf5b99ce59bfaa0498d635be02ccd335
LincRead <andre.l.read@gmail.com>
2017-10-16 10:04:07 +1000

    Fixed how sound plays based on distance to camera

---
+++ Assets/Scripts/UI/AudioListenerPosition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioListenerPosition : MonoBehaviour {
+
+    public Camera parentCamera;
+    Transform _transform;
+
+	void Start ()
+    {
+        _transform = GetComponent<Transform>();
+
+    }
+	
+	void Update ()
+    {
+        _transform.position = new Vector3(
+            CameraController.instance._transform.position.x, 
+            CameraController.instance._transform.position.y, 
+            CameraController.instance._transform.position.y);
+    }
+}
