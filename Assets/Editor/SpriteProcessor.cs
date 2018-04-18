9047e3c97a0943461d6a5e42072594af90975fdd
André Read <andre.l.read@gmail.com>
2018-04-18 13:50:47 +1000

    Added tooltip for stats icons and default values for imported sprites

---
+++ Assets/Editor/SpriteProcessor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.IO;
+
+public class SpriteProcessor : AssetPostprocessor
+{
+    void OnPreprocessTexture()
+    {
+        TextureImporter ti = (TextureImporter)assetImporter;
+        ti.textureType = TextureImporterType.Sprite;
+        ti.spriteImportMode = SpriteImportMode.Multiple;
+        ti.spritePixelsPerUnit = 100;
+        ti.mipmapEnabled = false;
+        ti.filterMode = FilterMode.Point;
+    }
+}
